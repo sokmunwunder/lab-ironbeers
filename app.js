200ab8e0c4b2d07fa937dfac285752ff56b77fc3
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-12-19 11:09:58 +0100

    Iteration 1 done

--- app.js
+++ app.js
@@ -13,13 +13,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    pageTitle: 'Home'
+  });
+});
+
+app.get('/beers', (req, res) => {
+  res.render('beers', {
+    pageTitle: 'Beers'
+  });
+});
+
+app.get('/random-beers', (req, res) => {
+  res.render('randomBeers', {
+    pageTitle: 'Random Beers'
+  });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
